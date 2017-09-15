a16a4bd44480a637021ba52ff8bf26e1f2c79f6c
doniatung <doniatung32@gmail.com>
2017-09-15 14:03:21 -0400

    makes a dictionary

--- filesOccupySpace.py
+++ filesOccupySpace.py
@@ -6,4 +6,28 @@ def read(csv):
 
 
 def makeDictionary(l):
+    d = {}
+    del l[0]
+    del l[-1]
+    for x in l:
+        if x.count(",") > 1:
+            quote = x.find('"', 1, len(x))
+            k = x[1 : quote]
+            v = float(x[quote + 2: -2])
+            d[k] = v
+        else:
+            comma = x.find(",")
+            k = x[: comma]
+            v = float(x[comma + 1: -2])
+            d[k] = v
+    return d 
+
+def pickOne(d):
     
+    
+    
+
+print makeDictionary(read("occupations.csv"))
+    
+    
+        
